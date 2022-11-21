1792da046510cc082dfe13b2cc7d36bf1a4d2cdd
Borrego Polo, Fernando <fbpolo@ea.com>
2022-11-21 19:49:53 +0100

    Add project files.

---
+++ Benchmark/Program.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+var summary = BenchmarkRunner.Run<MigrationTests>();
+
+[SimpleJob(RunStrategy.ColdStart, RuntimeMoniker.Net60, launchCount: 1, targetCount: 10)]
+[SimpleJob(RunStrategy.ColdStart, RuntimeMoniker.Net70, launchCount: 1, targetCount: 10)]
+public class MigrationTests
+{
+    [IterationSetup]
+    public void RemoveDatabase()
+    {
+        using var dbContext = new AdventureWorks2019Context();
+        dbContext.Database.EnsureDeleted();
+    }
+
+    [Benchmark]
+    public async Task Test()
+    {
+        using var dbContext = new AdventureWorks2019Context();
+        await dbContext.Database.MigrateAsync();
+    }
+}
