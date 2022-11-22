85b5d924730cc0dda57d4704558322a4f897e253
Borrego Polo, Fernando <fbpolo@ea.com>
2022-11-22 09:58:54 +0100

    Updated the Benchmark to include script generation. As it can be observed, the files are identical (except for the version).

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -4,24 +4,41 @@ using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 using Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 var summary = BenchmarkRunner.Run<MigrationTests>();
 
-[SimpleJob(RunStrategy.ColdStart, RuntimeMoniker.Net60, launchCount: 1, targetCount: 10)]
-[SimpleJob(RunStrategy.ColdStart, RuntimeMoniker.Net70, launchCount: 1, targetCount: 10)]
+[SimpleJob(RunStrategy.ColdStart, RuntimeMoniker.Net60, launchCount: 1, targetCount: 20)]
+[SimpleJob(RunStrategy.ColdStart, RuntimeMoniker.Net70, launchCount: 1, targetCount: 20)]
 public class MigrationTests
 {
+    private readonly string filename = $"../../../../../../../../migrations_{Environment.Version}.sql";
+
     [IterationSetup]
     public void RemoveDatabase()
     {
         using var dbContext = new AdventureWorks2019Context();
         dbContext.Database.EnsureDeleted();
+        if (File.Exists(filename))
+        {
+            File.Delete(filename);
+        }
     }
 
     [Benchmark]
-    public async Task Test()
+    public async Task Migration()
     {
         using var dbContext = new AdventureWorks2019Context();
         await dbContext.Database.MigrateAsync();
+    }
+
+    [Benchmark]
+    public async Task GenerateScript()
+    {
+        using var dbContext = new AdventureWorks2019Context();
+        var migrator = dbContext.GetService<IMigrator>();
+        var sql = migrator.GenerateScript();
+        await File.WriteAllTextAsync(filename, sql);
     }
 }
